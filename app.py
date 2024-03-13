1529e2d8754df881adc766b2036748596c951927
UssuyNeumann <luis.eduardo.un@hotmail.com>
2024-03-12 21:48:51 -0300

    Adicionada lista, estrutura de repetição for, tratamento de exceções e feito refatoração com funções.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
     print("""
 ███████╗ █████╗ ██████╗  ██████╗ ██████╗     ███████╗██╗  ██╗██████╗ ██████╗ ███████╗███████╗███████╗
@@ -18,31 +20,54 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
-    # os.system('clear')
-    print('Encerrando o sistema.\n')
+    exibir_subtitulo('Encerrando o sistema.')
 
 def opcao_invalida():
     print('Opção Inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal.')
+    voltar_menu_principal()
+
+def voltar_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal. ')
     main()
 
+def exibir_subtitulo(texto):
+    os.system('cls')
+    # os.system('clear')
+    print(texto)
+    print()
+
+def cadastrar_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_restaurante = input('Digite o nome do restaurante que deseja cadastrar: \n')
+    restaurantes.append(nome_restaurante)
+    print(f'\nO restaurante foi {nome_restaurante} cadastrado com sucesso!\n')
+    voltar_menu_principal()
+
+def listar_restaurante():
+    exibir_subtitulo('Lista de restaurantes cadastrados')
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    voltar_menu_principal()
+
 def escolher_opcao():
-    #snake_case
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao_escolhida}') #interpolação de strings
-    
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar Restaurante')
-        case 2:
-            print('Listar Restaurante')
-        case 3:
-            print('Ativar Restaurante')
-        case 4: 
-            finalizar_app()
-        case _:
-            opcao_invalida()
+    try:
+        #snake_case
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}') #interpolação de strings
+        
+        match opcao_escolhida:
+            case 1:
+                cadastrar_restaurante()
+            case 2:
+                listar_restaurante()
+            case 3:
+                print('Ativar Restaurante')
+            case 4: 
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida() 
 
 def main():
     os.system('cls')
